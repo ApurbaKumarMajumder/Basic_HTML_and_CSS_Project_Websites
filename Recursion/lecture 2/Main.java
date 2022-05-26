60dc09889d741d6bf740df1bb1703d91a5717229
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-26 16:27:10 +0530

    Tower Of Hanoi question

---
+++ Recursion/lecture 2/Main.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int scr = scn.nextInt();
+        int dest = scn.nextInt();
+        int helper = scn.nextInt();
+        toh(n, scr, dest, helper);
+    }
+
+    public static void toh(int n, int scr, int dest, int helper){
+        if(n == 0){
+            return;
+        }
+        toh(n-1, scr, helper, dest);
+        System.out.println(n+"["+scr+"->"+dest+"]");
+        toh(n-1, helper, dest, scr);
+    }
+// 41
+}
