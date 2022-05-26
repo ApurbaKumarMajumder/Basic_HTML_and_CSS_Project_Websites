1aa02077afe8740df456af5d22e5d688605f3b57
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-05-27 00:08:43 +0530

    get Stair Paths Question

--- Recursion/Lecture 4/Main.java
+++ Recursion/Lecture 4/Main.java
@@ -5,9 +5,13 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
-        String str = scn.nextLine();
-        ArrayList<String> res = gss(str);
+        // String str = scn.nextLine();
+        // ArrayList<String> res = gss(str);
+        // System.out.println(res);
+        int n = scn.nextInt();
+        ArrayList<String> res = getStairPaths(n);
         System.out.println(res);
+
     }
 
     public static ArrayList<String> gss(String str) {
@@ -33,5 +37,42 @@ public class Main {
         }
 
         return ans;
+    }
+
+    public static ArrayList<String> getStairPaths(int n) {
+        // This is a proactive approach
+        if(n == 0){
+            ArrayList<String> base = new ArrayList<>();
+            base.add("");
+            return base;
+        }
+
+        ArrayList<String> myPath = new ArrayList<>();
+
+        if(n >= 1){
+            // 1 step
+            ArrayList<String> path1Step = getStairPaths(n-1);
+            for (String path : path1Step) {
+                myPath.add('1' + path);
+            }
+        }
+
+        if(n >= 2){
+            // 2 steps
+            ArrayList<String> path2Step = getStairPaths(n-2);
+            for (String path : path2Step) {
+                myPath.add('2' + path);
+            }
+        }
+
+        if(n >= 3){
+            // 3 steps
+            ArrayList<String> path3Step = getStairPaths(n-3);
+            for (String path : path3Step) {
+                myPath.add('3' + path);
+            }
+        }
+
+        return myPath;
     }
 }
