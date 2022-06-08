9653b1e90fda04b1b10cf947e29b1209ace2510e
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-06-08 07:36:24 +0530

    print substring question

---
+++ Recursion/Lecture 5/Main.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        printSS(str, "");
+    }
+
+    public static void printSS(String str, String ans) {
+        if(str.length() == 0){
+            System.out.println(ans);
+            return;
+        }
+
+        char ch = str.charAt(0);
+        String restOfQuestion = str.substring(1);
+        printSS(restOfQuestion, ans+ch);
+        printSS(restOfQuestion, ans);
+    }
+}
