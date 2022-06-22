7cc698ecbc1adc111d872d90da4c6aaa20b60622
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-22 16:38:14 +0530

    flood fill question done

--- Recursion/Lecture 6/Main.java
+++ Recursion/Lecture 6/Main.java
@@ -4,9 +4,22 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        // printPermutations Questions 
+        // Scanner scn = new Scanner(System.in);
+        // String str = scn.nextLine();
+        // printPermutations(str, "");
+
+        // Flood Fill Questions
         Scanner scn = new Scanner(System.in);
-        String str = scn.nextLine();
-        printPermutations(str, "");
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        floodfill(arr, 0, 0, "");
     }
 
     public static void printPermutations(String str, String asf) {
@@ -25,4 +38,27 @@ public class Main {
         }
     }
 
+    // asf -> answer so far
+    public static void floodfill(int[][] maze, int row, int col, String asf) {
+        // negative base case
+        if(row < 0 || col < 0 || row >= maze.length || col >= maze[0].length || maze[row][col] == 1){
+            return;
+        }
+
+        if(row == maze.length - 1 && col == maze[0].length - 1){
+            System.out.println(asf);
+            return;
+        }
+
+        maze[row][col] = 1;
+        // top
+        floodfill(maze, row - 1, col, asf + "t");
+        // left
+        floodfill(maze, row, col - 1, asf + "l");
+        // bottom
+        floodfill(maze, row + 1, col, asf + "d");
+        // right
+        floodfill(maze, row, col + 1, asf + "r");
+        maze[row][col] = 0;
+    }
 }
