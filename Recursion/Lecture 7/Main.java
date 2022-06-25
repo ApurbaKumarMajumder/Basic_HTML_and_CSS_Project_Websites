21f3eeaf685be6aa7b8baa78d5f6cfffdde81893
Apurba Kumar Majumder <9851apurba@gmail.com>
2022-06-25 10:27:55 +0530

    knight tour

--- Recursion/Lecture 7/Main.java
+++ Recursion/Lecture 7/Main.java
@@ -10,7 +10,14 @@ public class Main {
         // printNQueens(arr, "", 0);
         
         // Knights Tour
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int row = scn.nextInt();
+        int col = scn.nextInt();
+
+        int[][] arr = new int[n][n];
+
+        printKnightsTour(arr, row, col, 0);
     }
 
     public static boolean isQueenSafe(int[][] arr, int row, int col){
@@ -51,5 +58,46 @@ public class Main {
                 chess[row][col] = 0;
             }
         }
+    }
+
+    public static void printKnightsTour(int[][] chess, int row, int col, int upcomingMove) {
+        // outside the chess board or the box is already visited
+        if(row < 0 || col < 0 || row >= chess.length || col >= chess[0].length || chess[row][col] > 0){
+            return;
+        }
+
+        // base case for chess board completion
+        if(upcomingMove == chess.length * chess[0].length){
+            chess[row][col] = upcomingMove;
+            displayBoard(chess);
+            chess[row][col] = 0;
+            return;
+        }
+
+        // mark the move you have done
+        chess[row][col] = upcomingMove;
+
+        // knight has 8 different moves
+        printKnightsTour(chess, row - 2, col + 1 , upcomingMove + 1);
+        printKnightsTour(chess, row - 1, col + 2 , upcomingMove + 1);
+        printKnightsTour(chess, row + 1, col + 2 , upcomingMove + 1);
+        printKnightsTour(chess, row + 2, col + 1 , upcomingMove + 1);
+        printKnightsTour(chess, row + 2, col - 1 , upcomingMove + 1);
+        printKnightsTour(chess, row + 1, col - 2 , upcomingMove + 1);
+        printKnightsTour(chess, row - 1, col - 2 , upcomingMove + 1);
+        printKnightsTour(chess, row - 2, col - 1 , upcomingMove + 1);
+
+        // unmark the move
+        chess[row][col] = 0;
+    }
+
+    public static void displayBoard(int[][] chess){
+        for(int i = 0; i < chess.length; i++){
+            for(int j = 0; j < chess[0].length; j++){
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
