e24092bfed4b1d3b034e371ab84ed3ac35d73965
Apurba Kumar Majumder <65047407+ApurbaKumarMajumder@users.noreply.github.com>
2022-08-03 01:19:14 +0530

    preorder, postorder and inorder traversal

---
+++ Binary Tree/intro.java
@@ -0,0 +1,80 @@
+import java.io.*;
+import java.util.*;
+
+public class intro {
+
+    static class Node {
+
+        int data;
+        Node left;
+        Node right;
+
+        Node (int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static class BinaryTree {
+        static int idx = -1;
+        public static Node buildTree(int nodes[]) {
+            idx++;
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+            return newNode;
+        }
+    }
+
+    public static void preOrder(Node root) {
+
+        // root left and right
+        if (root == null) {
+            return;
+        }
+
+        System.out.print(root.data + " ");
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    public static void inOrder(Node root) {
+
+        // left, root and right
+        if (root == null) {
+            return;
+        }
+
+        inOrder(root.left);
+        System.out.print(root.data + " ");
+        inOrder(root.right);
+    }
+
+    public static void postOrder(Node root) {
+
+        // left, right and root
+        if (root == null) {
+            return;
+        }
+
+        postOrder(root.left);
+        postOrder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void main(String[] args) {
+        
+        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        BinaryTree tree = new BinaryTree();
+
+        Node root = tree.buildTree(nodes);
+        System.out.println(root.data);
+
+        postOrder(root);
+    }
+}
--- Stacks And Queues/Next Greater Element To The Right/Main.java
+++ Stacks And Queues/Next Greater Element To The Right/Main.java
@@ -32,6 +32,7 @@ public class Main{
         st.push(arr[n-1]);
 
         for (int i=n-2; i>=0; i--) {
+            // pop, print, push
             while (st.size() > 0 && arr[i] >=st.peek()) {
                 st.pop();
             }
