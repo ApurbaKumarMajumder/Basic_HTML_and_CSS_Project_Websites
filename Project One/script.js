2859fa88e1229c7d9409e682a875ed20d42ebcc8
Apurba Kumar Majumder <9851apurba@gmail.com>
2023-09-30 04:13:33 +0530

    project done Well Done

--- Project One/script.js
+++ Project One/script.js
@@ -29,7 +29,7 @@ gsap.to("#nav", {
   scrollTrigger: {
     trigger: "#nav",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top -10%",
     end: "top -11%",
     scrub: 1
@@ -41,7 +41,7 @@ gsap.to("#main", {
   scrollTrigger: {
     trigger: "#main",
     scroller: "body",
-    markers: true,
+    // markers: true,
     start: "top -25%",
     end: "top -70%",
     scrub: 2
